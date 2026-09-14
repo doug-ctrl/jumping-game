ebb8d25bd8f80b9b2fcd1ec3298650a4025d1d3c
deaavdulaj <deaavd@gmail.com>
2026-09-14 15:57:20 +0200

    add player jump function and style

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ const GROUND_Y = 0;
 let velocityY = 0;
 let playerBottom = GROUND_Y;
 let isJumping = false;
+
+function jump() {
+  if (isJumping) return;
+  isJumping = true;
+  velocityY = JUMP_VELOCITY;
+}
+
+function updatePlayer() {
+  if (!isJumping) return;
+
+  velocityY += GRAVITY;
+  playerBottom -= velocityY;
+
+  if (playerBottom <= GROUND_Y) {
+    playerBottom = GROUND_Y;
+    velocityY = 0;
+    isJumping = false;
+  }
+
+  player.style.bottom = `${30 + playerBottom}px`;
+}
+
+function loop() {
+  updatePlayer();
+  requestAnimationFrame(loop);
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Space') {
+    e.preventDefault();
+    jump();
+  }
+});
+
+requestAnimationFrame(loop);
